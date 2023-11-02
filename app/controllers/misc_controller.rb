4118a20dcb8be899bccedf7c2913b48724e039d1
fabletlc373 <51000618+fabletlc373@users.noreply.github.com>
2023-11-02 03:07:27 +0000

    initial checkin

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,43 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def all_directors
+    @d = Director.all
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def director_details
+    @did = params.fetch('id')
+    @ddetails = Director.all.where({:id => @did}).first
+    @movies = Movie.all.where({:director_id => @did})
+    render({ :template => "misc_templates/directordetails"})
+  end
+
+  def all_movies
+    @d = Director.all
+    @m = Movie.all
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def movie_details
+    @mid = params.fetch('id')
+    @m = Movie.all.where({:id => @mid}).first
+    @d = Director.all.where({:id => @m.director_id}).first
+    
+    render({ :template => "misc_templates/moviedetails"})
+  end
+
+  def all_actors
+    @a = Actor.all
+    render({ :template => "misc_templates/actors"})
+  end
+
+  def actor_details
+    @aid = params.fetch('id')
+    @adetails = Actor.all.where({:id => @aid}).first
+    @movies = Character.all.where({:actor_id => @aid})
+    render({ :template => "misc_templates/actordetails"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "all_directors"})
+  get("/directors/:id", { :controller => "misc", :action => "director_details"})
+
+  get("/movies", { :controller => "misc", :action => "all_movies"})
+  get("/movies/:id", { :controller => "misc", :action => "movie_details"})
+  
+  get("/actors", { :controller => "misc", :action => "all_actors"})
+  get("/actors/:id", { :controller => "misc", :action => "actor_details"})
+  
+  #get("/directors/youngest")
+  #get("/directors/eldest")
+  #get("/movies")
+  #get("/actors")
 end
